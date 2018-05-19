cf30f0fa92bcccc1abe2070244310aeccd85d1be
Bago <jonas.kris.collier@gmail.com>
2018-05-19 09:14:13 +0200

    Update coins.py

--- env/coins.py
+++ env/coins.py
@@ -1,27 +1,30 @@
 
 
 
-class Zaobi(Coin):
-    NAME = "Zaobi"
-    SHORTNAME = "ZB"
+class Berycoin(Coin):
+    NAME = "Berycoin"
+    SHORTNAME = "BERY"
     NET = "mainnet"
-    XPUB_VERBYTES = bytes.fromhex("0488B21E")
-    XPRV_VERBYTES = bytes.fromhex("0488ADE4")
-    P2PKH_VERBYTE = bytes.fromhex("36")
-    P2SH_VERBYTES = [bytes.fromhex("08")]
-    WIF_BYTE = bytes.fromhex("99")
-    GENESIS_HASH = ('00060070755e09d835e3627a000d7064'
-                    '662a91c3d2c297a2acb46124e1402511')
-    DESERIALIZER = DeserializerTxTime
-    DAEMON = LegacyRPCDaemon
-    TX_COUNT = 34999
-    TX_COUNT_HEIGHT = 30070
-    TX_PER_BLOCK = 1
-    IRC_PREFIX = "E_"
-    IRC_CHANNEL = "#electrum-blk"
-    RPC_PORT = 7294
-    REORG_LIMIT = 5000
+    XPUB_VERBYTES = bytes.fromhex("0586c22e")
+    XPRV_VERBYTES = bytes.fromhex("0586dcf1")
+    P2PKH_VERBYTE = bytes.fromhex("19")
+    P2SH_VERBYTES = [bytes.fromhex("33"), bytes.fromhex("14")]
+    WIF_BYTE = bytes.fromhex("d8")
+    GENESIS_HASH = ('ff23a3590cc6e308f1817283cfa80779'
+                    '3aae41d6a1de9f30ed15b7e8361d7005')
+    DESERIALIZER = lib_tx.DeserializerSegWit
+    DAEMON = daemon.LegacyRPCDaemon
+    TX_COUNT = 26906
+    TX_COUNT_HEIGHT = 24916
+    TX_PER_BLOCK = 10
+    RPC_PORT = 9432
+    REORG_LIMIT = 800
     HEADER_HASH = None
+    PEERS = [
+        'electrum.berycoin.com s t',
+        'electrum.resteemexposure.com s t',
+        'electrum2.berycoin.com s t',        
+    ]
 
     @classmethod
     def header_hash(cls, header):
