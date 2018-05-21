1320c4150b02283cc95bdacd9f310e8048e9e38c
Bago <jonas.kris.collier@gmail.com>
2018-05-21 12:21:20 +0200

    Update coins.py

--- env/coins.py
+++ env/coins.py
@@ -24,18 +24,18 @@ class Berycoin(Coin):
         'electrum2.berycoin.com s t',        
     ]
 
-    @classmethod
-    def header_hash(cls, header):
-        '''Given a header return the hash.'''
-        if cls.HEADER_HASH is None:
-            import scrypt
-            cls.HEADER_HASH = lambda x: scrypt.hash(x, x, 1024, 1, 1, 32)
+#    @classmethod
+#    def header_hash(cls, header):
+#        '''Given a header return the hash.'''
+#        if cls.HEADER_HASH is None:
+#            import scrypt
+#            cls.HEADER_HASH = lambda x: scrypt.hash(x, x, 1024, 1, 1, 32)
 
-        version, = struct.unpack('<I', header[:4])
-        if version > 6:
-            return super().header_hash(header)
-        else:
-            return cls.HEADER_HASH(header);
+#        version, = struct.unpack('<I', header[:4])
+#        if version > 6:
+#            return super().header_hash(header)
+#        else:
+#            return cls.HEADER_HASH(header);
 
 
 class BerycoinTestnet(Berycoin):
